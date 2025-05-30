debe92667af75fae059ea11b060c8e032a3c931b
Andre Calderon <gerardo.andree07@gmail.com>
2025-05-30 13:56:35 -0600

    Actualizado el trabajo

--- Trabajos de clase 9/Eliminandovalore.py
+++ Trabajos de clase 9/Eliminandovalore.py
@@ -3,10 +3,9 @@ numeroslista = numerosusuario.split()
 numerosenteroslista = [int(n) for n in numeroslista]
 
 resultado = []
-numerosyaaparecidos = set()
-for n in numerosenteroslista:
-    if n not in numerosyaaparecidos:
-        resultado.append(n)
-        numerosyaaparecidos.add(n)
 
-print("La lista de numeros sin duplicados es:", resultado)
+for n in numerosenteroslista:
+    if n not in resultado:
+        resultado.append(n)
+
+print("La lista de numeros sin duplicados es:", resultado) 
